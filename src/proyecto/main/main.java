f84575638b7a6b7e8472c482f11bb3875750fa1b
Bryan Paez <bryanpaez.125@gmail.com>
2022-08-12 20:37:13 -0600

    Se cambiaron propiedades del proyecto

--- src/proyecto/main/main.java
+++ src/proyecto/main/main.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package compi1proyecto1.pkg201700945;
+package proyecto.main;
 
 /**
  *
